50bfeb8d4edd6784e709d81fa67e7d664f999bfd
Sung Paik <sung.paik@codeimmersives.com>
2019-11-18 14:29:54 -0500

    cleaned up syntax

--- main.js
+++ main.js
@@ -19,19 +19,17 @@ function setAge(person, age) {
 }
 
 function giveBirthday(person) {
-  return (person.age) ? person.age = person.age + 1 : person.age = 1;
+  return person.age ? person.age = person.age + 1 : person.age = 1;
 }
 
 function marry(person1, person2) {
-  person1.married = true;
-  person2.married = true;
+  person1.married = true, person2.married = true;
   person1.spouseName = `${person2.firstName} ${person2.lastName}`;
   person2.spouseName = `${person1.firstName} ${person1.lastName}`;
 }
 
 function divorce(person1, person2) {
-  person1.married = false;
-  person2.married = false;
+  person1.married = false, person2.married = false;
   delete person1.spouseName;
   delete person2.spouseName;
 }
